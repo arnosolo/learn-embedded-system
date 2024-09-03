9ee3b26e68390c4eb57d429aa1eaf508e506b4b5
Arno Solo <arno756@outlook.com>
2024-09-03 18:19:13 +0800

    Update docs/zh/51-mcu-timer.md

--- codes/demo204-51-timer/src/main.c
+++ codes/demo204-51-timer/src/main.c
@@ -1,6 +1,5 @@
 #include <8052.h>
 #include "led.h"
-#include "utils.h"
 
 #define EXT_OSCILLATOR_FREQUENCY  11059200L
 // Counter number in 1ms (12T mode)
@@ -9,9 +8,16 @@
 
 int count = 0;
 
-/* Timer0 interrupt routine */
-// Note: __interrupt (1) is a SDCC specific keyword,
-// other compilers use different keywords
+void split_16bit_number(uint16_t number, uint8_t *highByte, uint8_t *lowByte) {
+  *highByte = (number >> 8) & 0xFF; // Extract the high byte
+  *lowByte = number & 0xFF;         // Extract the low byte
+}
+
+/*
+ * Timer0 interrupt routine
+ * Note: __interrupt (1) is a SDCC specific keyword,
+ * other compilers use different keywords
+ */
 void timer0_isr(void) __interrupt (1) {
   uint8_t highByte, lowByte;
   split_16bit_number(TIMER_1_START_NUM, &highByte, &lowByte);
@@ -30,14 +36,14 @@ void setup_timer0(void) {
   uint8_t highByte, lowByte;
   split_16bit_number(TIMER_1_START_NUM, &highByte, &lowByte);
 
-  TMOD &= 0xF0;  // Clear the lower 4 bits of TMOD,
-  TMOD |= 0x01;  // Set the lower 4 bits of TMOD as 0x1, Timer 0, internal oscillator, mode1 (16-bit)
-  TL0 = lowByte;  // Set Timer 0 low byte
+  TMOD &= 0xF0;     // Clear the lower 4 bits of TMOD,
+  TMOD |= 0x01;     // Timer 0, internal oscillator, mode1 (16-bit)
+  TL0 = lowByte;    // Set Timer 0 low byte
   TH0 = highByte;   // Set Timer 0 high byte
-  TR0 = 1; // Start Timer 0
+  TR0 = 1;          // Start Timer 0
 
-  EA = 1;  // Enable global interrupts
-  ET0 = 1; // Enable Timer 0 interrupt
+  EA = 1;           // Enable global interrupts
+  ET0 = 1;          // Enable Timer 0 interrupt
 }
 
 void main(void) {
